b0465887e4f2e5af458d8f868e4dbe2f493d1a57
Γιώργος Καϊμακάς <gkaimakas@georges-macbook-pro.local>
2018-11-08 12:06:06 +0200

    Initial Commit

---
+++ MovieRama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieRama
+//
+//  Created by George Kaimakas on 08/11/2018.
+//  Copyright © 2018 George Kaimakas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
